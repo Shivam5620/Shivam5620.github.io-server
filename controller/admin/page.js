47c33f630dda2bac54cd2f8ec0775c2e840c96ba
Shivam5620 <sbirla04082000@gmail.com>
2023-12-20 15:58:50 +0530

    update this code

--- controller/admin/page.js
+++ controller/admin/page.js
@@ -1,13 +1,15 @@
 const Page = require("../../models/page");
 
-exports.createPage = (req, res) => {
+exports.createPage = async (req, res) => {
   const { banners, products } = req.files;
+  // Process banners
   if (banners && banners.length > 0) {
     req.body.banners = banners.map((banner, index) => ({
       img: `/public/${banner.filename}`,
       navigateTo: `/bannerClicked?categoryId=${req.body.category}&type=${req.body.type}`,
     }));
   }
+  // Process products
   if (products && products.length > 0) {
     req.body.products = products.map((product, index) => ({
       img: `/public/${product.filename}`,
@@ -17,36 +19,36 @@ exports.createPage = (req, res) => {
 
   req.body.createdBy = req.user._id;
 
-  Page.findOne({ category: req.body.category }).exec((error, page) => {
-    if (error) return res.status(400).json({ error });
+  try {
+    let page = await Page.findOne({ category: req.body.category });
+    
     if (page) {
-      Page.findOneAndUpdate({ category: req.body.category }, req.body).exec(
-        (error, updatedPage) => {
-          if (error) return res.status(400).json({ error });
-          if (updatedPage) {
-            return res.status(201).json({ page: updatedPage });
-          }
-        }
-      );
+      page = await Page.findOneAndUpdate({ category: req.body.category }, req.body, { new: true }).exec();
+      return res.status(201).json({ page });
     } else {
-      const page = new Page(req.body);
-
-      page.save((error, page) => {
-        if (error) return res.status(400).json({ error });
-        if (page) {
-          return res.status(201).json({ page });
-        }
-      });
+      const newPage = new Page(req.body);
+      page = await newPage.save();
+      return res.status(201).json({ page });
     }
-  });
-};
-
-exports.getPage = (req, res) => {
-  const { category, type } = req.params;
-  if (type === "page") {
-    Page.findOne({ category: category }).exec((error, page) => {
-      if (error) return res.status(400).json({ error });
-      if (page) return res.status(200).json({ page });
-    });
+  } catch (error) {
+    return res.status(400).json({ error: error.message || "An error occurred" });
   }
 };
+
+
+exports.getPage = async (req, res) => {
+  const { category, type } = req.params;
+  if (type === "page") {
+    try {
+      const page = await Page.findOne({ category });
+      if (page) {
+        return res.status(200).json({ page });
+      } else {
+        return res.status(404).json({ error: "Page not found" });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  }
+};
+
